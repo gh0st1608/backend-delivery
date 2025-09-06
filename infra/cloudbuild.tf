cfd168b6aa02009f1fe62ff1ec2f113899e84f0e
gh0st1608 <egalindoa@uni.pe>
2025-09-06 13:35:05 -0500

    add codebuild project design

--- infra/cloudbuild.tf
+++ infra/cloudbuild.tf
@@ -1,25 +1,44 @@
 resource "aws_codebuild_project" "design" {
   name          = "build-design-foodstore"
-  description   = "CodeBuild project for design pipeline"
-  build_timeout = 5
-
-  service_role = "arn:aws:iam::248268265208:role/service-role/codebuild-foodstore-build-service-role"
+  description   = ""
+  build_timeout = 15
+  service_role  = "arn:aws:iam::248268265208:role/service-role/codebuild-build-design-foodstore-service-role"
 
   artifacts {
     type = "NO_ARTIFACTS"
   }
 
+  cache {
+    type = "NO_CACHE"
+  }
+
   environment {
-    compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/standard:5.0"
-    type                        = "LINUX_CONTAINER"
+    compute_type                = "BUILD_LAMBDA_4GB"
+    image                       = "aws/codebuild/amazonlinux-x86_64-lambda-standard:nodejs20"
+    type                        = "LINUX_LAMBDA_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
+    privileged_mode             = false
+  }
+
+  logs_config {
+    cloudwatch_logs {
+      status     = "ENABLED"
+      group_name = ""
+      stream_name = ""
+    }
+    s3_logs {
+      status = "DISABLED"
+    }
   }
 
   source {
     type            = "GITHUB"
-    location        = "https://github.com/gh0st1608/backend-delivery.git"
+    location        = "https://github.com/gh0st1608/backend-delivery"
     buildspec       = "api/deploy/buildspec.yaml"
     git_clone_depth = 1
+
+    git_submodules_config {
+      fetch_submodules = false
+    }
   }
 }
