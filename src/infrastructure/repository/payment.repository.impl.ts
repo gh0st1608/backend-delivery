c9d5ddbff1de7f1020763d826918b12d5c424ed2
gh0st1608 <egalindoa@uni.pe>
2026-06-01 00:47:57 -0500

    fix final

--- src/infrastructure/repository/payment.repository.impl.ts
+++ src/infrastructure/repository/payment.repository.impl.ts
@@ -25,7 +25,7 @@ export class PaymentRepositoryImpl implements PaymentRepository {
   constructor() {
     this.docClient = DynamoDBDocumentClient.from(
       new DynamoDBClient({
-        region: process.env.REGION ?? 'us-east-1',
+        region: process.env.AWS_REGION ?? 'us-east-1',
         credentials: getAwsCredentials(),
       }),
       {
